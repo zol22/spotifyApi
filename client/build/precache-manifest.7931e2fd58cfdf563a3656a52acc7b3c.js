a4c7b738bfa087d6e189163a46caa2c0b804d517
zol <31492461+zol22@users.noreply.github.com>
2019-08-14 14:55:47 -0400

    Add files via upload

---
+++ client/build/precache-manifest.7931e2fd58cfdf563a3656a52acc7b3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c81e79e1b716486169908678c41b10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "483ee97983cdc265b44d",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "2935e2e445e7ba29c9f1",
+    "url": "/static/css/main.375c66ae.chunk.css"
+  },
+  {
+    "revision": "483ee97983cdc265b44d",
+    "url": "/static/js/2.4251a437.chunk.js"
+  },
+  {
+    "revision": "2935e2e445e7ba29c9f1",
+    "url": "/static/js/main.8871a06b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
